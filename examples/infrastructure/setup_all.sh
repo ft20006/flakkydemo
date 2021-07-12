3d5270eed480325408647e553f6e67487c6be8a3
EC2 Default User <ec2-user@ip-10-10-5-217.ec2.internal>
2021-07-12 22:28:38 +0000

    modify

--- examples/infrastructure/setup_all.sh
+++ examples/infrastructure/setup_all.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-export AWS_ACCOUNT_ID=425910847289
-export KEY_PAIR_NAME=demo1-kp
+export AWS_ACCOUNT_ID=967678641736
+export KEY_PAIR_NAME=demokp
 export AWS_PROFILE=default
 export AWS_DEFAULT_REGION=us-east-1
 export ENVIRONMENT_NAME=DEMO
@@ -17,12 +17,12 @@ export COLORTELLER_ECR_REPO_NAME=colorteller
 export ENVOY_IMAGE=public.ecr.aws/appmesh/aws-appmesh-envoy:v1.17.2.0-prod
 export CLUSTER_SIZE=6
 export CLUSTER_INSTANCE_TYPE=t2.micro
-export TASK_IAM_ROLE=arn:aws:iam::425910847289:role/Container-IRAD-Task2-Roles-TaskIamRole-M4ABSROVAAJD
-export TASK_EXECUTION_ROLE=arn:aws:iam::425910847289:role/Container-IRAD-Task2-Roles-TaskExecutionIamRole-7E9FWGRXQ2NW
-export NODE_INSTANCE_ROLE=Container-IRAD-Task2-Roles-NodeInstanceRole-156NQXTU077T0
-export ECS_INSTANCE_ROLE=Container-IRAD-Task2-Roles-ECSInstanceRole-1FOJVLY2J16P3
-export ECS_SERVICE_AUTO_SCALING_ROLE=arn:aws:iam::425910847289:role/Container-IRAD-Task2-Role-ECSServiceAutoScalingRol-57MD0JVEQ0I7
-export AWS_SERVICES_ROLE_AMAZON_EKS=arn:aws:iam::425910847289:role/Container-IRAD-Task2-Role-AWSServiceRoleForAmazonE-1NPI9HQSI6TSX
+export TASK_IAM_ROLE=arn:aws:iam::967678641736:role/ContainerIrad-roles-TaskIamRole-RTDHLGKZKYLV
+export TASK_EXECUTION_ROLE=arn:aws:iam::967678641736:role/ContainerIrad-roles-TaskExecutionIamRole-48L72V9WWRLP
+export NODE_INSTANCE_ROLE=ContainerIrad-roles-NodeInstanceRole-13QIV1WWV77M5
+export ECS_INSTANCE_ROLE=ContainerIrad-roles-ECSInstanceRole-1NLOCWE3VRB9T
+export ECS_SERVICE_AUTO_SCALING_ROLE=arn:aws:iam::967678641736:role/ContainerIrad-roles-ECSServiceAutoScalingRole-TLKVOK8ICWJ3
+export AWS_SERVICES_ROLE_AMAZON_EKS=arn:aws:iam::967678641736:role/ContainerIrad-roles-AWSServiceRoleForAmazonEKS-1EZLPMVE4RB0A
 
 ## run ./examples/infrastructure/setup_all.sh
 
